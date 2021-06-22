b9aa682991a19c9a3af4dc209269c6450d8d8db8
n00bi3es <39062772+widhisec@users.noreply.github.com>
2021-06-22 23:37:27 +0700

    Add files via upload

--- likeig.sh
+++ likeig.sh
@@ -11,34 +11,49 @@ N=$(tput sgr0)
 B=$(tput setaf 6)
 Y=$(tput setaf 3)
 time=$(date +%s%N|cut -b1-13)
+function random(){
+               OLDIFS=$IFS
+IFS=$(echo -en "\n\b")
+create="akun.txt"
+for file in $(awk 'END {print NR}' $create)
+do
+    randomNumber=$(($RANDOM*$file))
+done
+IFS=$OLDIFS
+}
 function login() {
-	      printf "\n${H}[+]${N}Logining in..\n"
-        ambil=$(curl -D - 'https://www.instagram.com/accounts/login/' \
-         -H "user-agent: $useragent" \
-         -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
-         -H 'accept-encoding: gzip, deflate, br' \
-         -H 'accept-language: en-US,en;q=0.9' --compressed -s)
-         csrf=$(echo "$ambil" | grep -Po '(?<=csrftoken=)[^;]*')
-         login=$(curl -D - 'https://www.instagram.com/accounts/login/ajax/' \
-         -H 'origin: https://www.instagram.com' \
-         -H 'x-requested-with: XMLHttpRequest' \
-         -H "user-agent: $useragent" \
-         -H "x-csrftoken: $csrf" \
-         -H 'content-type: application/x-www-form-urlencoded' \
-         -H 'accept: */*' \
-         -H 'referer: https://www.instagram.com/accounts/login/' --data-urlencode "username=${1}" --data-urlencode "enc_password=#PWD_INSTAGRAM_BROWSER:0:${time}:${2}" --data-urlencode "optIntoOneTap=false" --compressed -sL)     
-        check=$(echo -e "$login" | grep -Po '(?<=checkpoint_url":")[^"]*')
-        usid=$(echo -e "$login" | grep -Po '(?<=userId":")[^"]*')
-        isauth=$(echo -e "$login" | grep -Po '(?<="authenticated":)[^,]*')
-        session=$(echo -e "$login" | grep -Po '(?<=sessionid=)[^;]*')
-	err=$(echo -e "$login" |grep -Po '(?<=message":")[^"]*')
-        if [[ $isauth =~ "true" ]];then
-            printf "${H}[+]${N}Login Success..\n"
-            printf "${H}[+]${N}User ID : $usid\n"
-        else
-	echo  $err
-            printf "[-]${M}GAGAL LOGIN]${N}\n"
+         find=$(find *.cookies -type f)
+         if [[ -n "$find" ]]; then
+            random
+            printf "\n${H}[+]${N}Logining in..\n"
+            local ambil=$(curl -D - 'https://www.instagram.com/accounts/login/' \
+                  -H "user-agent: $useragent" \
+                  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
+                  -H 'accept-encoding: gzip, deflate, br' \
+                  -H 'accept-language: en-US,en;q=0.9' --compressed -sL)
+            local csrf=$(echo "$ambil" | grep -Po '(?<=csrftoken=)[^;]*')
+            local mid=$(echo "$ambil"|grep -Po '(?<=mid=)[^;]*')
+            local did=$(echo "$ambil"|grep -Po '(?<=ig_did=)[^;]*')
+            local ses=$(echo "$ambil"|grep -Po '(?<=sessionid=)[^;]*')
+            login=$(curl -D - 'https://www.instagram.com/accounts/login/ajax/' \
+                   -H 'origin: https://www.instagram.com' \
+                   -H 'x-requested-with: XMLHttpRequest' \
+                   -H "user-agent: $useragent" \
+                   -H "x-csrftoken: $csrf" \
+                   -H 'content-type: application/x-www-form-urlencoded' \
+                   -H 'accept: */*' \
+                   -H 'referer: https://www.instagram.com/accounts/login/' --data-urlencode "username=${1}" --data-urlencode "enc_password=#PWD_INSTAGRAM_BROWSER:0:${time}:${2}" --data-urlencode "optIntoOneTap=false" --compressed -sL)     
+        local check=$(echo -e "$login" | grep -Po '(?<=checkpoint_url":")[^"]*')
+        local usid=$(echo -e "$login" | grep -Po '(?<=userId":")[^"]*'|sort -u)
+        local isauth=$(echo -e "$login" | grep -Po '(?<="authenticated":)[^,]*')
+        local session=$(echo -e "$login" | grep -Po '(?<=sessionid=)[^;]*')
+        echo "${login}" >> $randomNumber.cookies
+    else
+        gete
+            
         fi
+	      
+        
 }
 function idd() { 
 	    curl -s "${URL}" --location -A "${useragent}" \
@@ -48,8 +63,23 @@ function idd() {
         -H "x-csrftoken: $csrf" \
         -H "x-instagram-ajax: $rolout" \
         -H 'content-type: application/x-www-form-urlencoded' \
-        -H 'accept: */*' --compressed|
-        grep 'instagram://media?id=' | grep -oP 'id=\K.*(?=")'
+        -H 'accept: */*' --compressed|grep -Po '(?<=__typename":"GraphImage","id":").*?(?=")'
+}
+function oke(){
+          Fruits=('csrftoken' 'ig_did' 'sessionid')
+          Fruits[0]="csrftoken"
+          Fruits[1]="ig_did"
+          Fruits[2]="sessionid"
+          find=$(find *.cookies -type f)
+         for i in "${find[@]}"; do
+           local x=$(echo "${Fruits[0]}")
+           local b=$(echo "${Fruits[1]}")
+           local p=$(echo "${Fruits[2]}")
+           local c=$(cat $i|grep -Po "(?<=$x=)[^;]*")
+           local d=$(cat $i|grep -Po "(?<=$b=)[^;]*")
+           local s=$(cat $i|grep -Po "(?<=$p=)[^;]*")
+           for haha in $c;do echo $haha;done
+         done
 }
 function open(){
           for URL in $(cat fot.txt); do
@@ -57,19 +87,25 @@ function open(){
           done
 }
 function gete(){
+  oke
+  echo "${d[*]}"
+   csrf=$(cat 1793.cookies| grep -Po '(?<=csrftoken=)[^;]*')
+ did=$(cat 1793.cookies|grep -Po '(?<=ig_did=)[^;]*')
+ ses=$(cat 1793.cookies|grep -Po '(?<=sessionid=)[^;]*')
       v=$(open)
        echo -e "$v" >> NAH.txt
        for ea in $(cat NAH.txt);do
-       x=$(curl -s "https://www.instagram.com/web/likes/$ea/like/" -X 'POST' \
-           -H 'origin: https://www.instagram.com' \
-           -A "${useragent}" \
-           -H 'accept-language: en-US,en;q=0.9' \
-           -H 'x-requested-with: XMLHttpRequest' \
-           -H "csrftoken=$csrf" \
-           -H "x-csrftoken: $csrf" \
-           -H 'content-type: application/x-www-form-urlencoded' --compressed -L)
-        status=$(echo -e "$x" | grep -Po '(?<=status": ")[^"]*')
-                 printf "${H}[#]${N}$status (mdaID) -> $ea${N}\n"
+           x=$(curl "https://www.instagram.com/web/likes/${ea}/like/" \
+  -X 'POST' \
+  -H 'content-type: application/x-www-form-urlencoded' \
+  -H 'accept: */*' \
+  -H 'x-requested-with: XMLHttpRequest' \
+  -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36" \
+  -H "x-csrftoken: $csrf" \
+  -H "cookie: ig_did=${did};rur=VLL; csrftoken=${csrf}; ds_user_id=${usid}; sessionid=${ses}" --compressed -s
+)
+        status=$(echo -e "$x" |grep -Po '(?<=status":")[^"]*')
+                 printf "${H}[#]${N}$x (mdaID) -> $ea${N}\n"
     done
     rm -rf NAH.txt
 }
@@ -102,8 +138,7 @@ kamu=$(expr $hitung % 1)
 if [[ $kamu == 0 && $hitung > 0 ]]; then
 sleep 1
 fi
-login $EMAIL $PASS $penambahan $number
-gete &
+login $EMAIL $PASS $penambahan $number &
 hitung=$[$hitung+1]
 done
 wait
