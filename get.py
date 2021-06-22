6c9c29f02375738c282f8a7d4a36926e30b86fd7
n00bi3es <39062772+widhisec@users.noreply.github.com>
2021-06-23 02:42:38 +0700

    Update get.py

--- get.py
+++ get.py
@@ -2,6 +2,9 @@ try:
 	import requests,json,sys,re
 except Exception as oke:
 	raise (oke)
+if sys.version[0] in '2':
+   print('[x] No Supported For python 2.x Please Use Python 3.x \n')
+   exit(1)
 def biasalah(url=True):
     with requests.Session() as req:
     	ses = req.get(url, cookies={"csrftoken":sys.argv[1]})
