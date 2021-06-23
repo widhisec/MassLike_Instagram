a664277a9305ced3b68fc3c4f29866cf8fe957d2
n00bi3es <39062772+widhisec@users.noreply.github.com>
2021-06-23 21:52:21 +0700

    Update likeig.sh

--- likeig.sh
+++ likeig.sh
@@ -142,7 +142,7 @@ EMAIL=${array[0]}
 PASS=${array[1]}
 NUMBER=$((nomor++))
 kamu=$(expr $hitung % 1)
-if [[ $kamu == 0 && $hitung > 0 ]]; then
+if [[  $kamu -eq 0 || $hitung -eq 1 ]]; then
 sleep 1
 fi
 login $EMAIL $PASS $NUMBER &
