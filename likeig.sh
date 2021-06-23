18940c1caa3cfacab806708487fb39e64195e4ae
n00bi3es <39062772+widhisec@users.noreply.github.com>
2021-06-23 14:49:05 +0700

    Update likeig.sh

--- likeig.sh
+++ likeig.sh
@@ -27,13 +27,14 @@ function random(){
                IFS=$OLDIFS
 }
 function login() {
-          i='/data/data/com.termux/files/home/MassLike_Instagram'
-          j='*.cookies'
-          y=$i/$j                                                           
-	  readarray -t files < <(compgen -G "$y") # 
+          local SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+          #default='/data/data/com.termux/files/home/MassLike_Instagram'
+          COOKIE_MY='*.cookies'
+          local y=${SCRIPT_DIR}/${COOKIE_MY}                                                          
+	  (readarray -t files < <(compgen -G "$y")) 
          #find=$(find *.cookies -type f)
          if [[ ! -e $files ]];then                                                 
-	    for i in "${files[@]}";do echo "Found -$i-";done
+	    for i in "${files[@]}";do echo "Found -${SCRIPT_DIR}-";done
             random
             printf "\n%s[+]%sLogining in..\n" "${H}" "${N}"
             local ambil=$(curl -D - 'https://www.instagram.com/accounts/login/' \
