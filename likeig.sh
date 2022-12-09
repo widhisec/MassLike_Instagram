39e326d8eab7932891d477d2e173f5ee07c8e927
widhi satrio <39062772+widhisec@users.noreply.github.com>
2022-12-09 20:24:31 +0700

    Update likeig.sh

--- likeig.sh
+++ likeig.sh
@@ -1,175 +1,137 @@
-						 echo "${login}" >> $randomNumber.cookies
-						 echo "Found -${SCRIPT_DIR}-";
-						 local check=$(echo -e "$login" | grep -Po '(?<=checkpoint_url":")[^"]*')
-						 local isauth=$(echo -e "$login" | grep -Po '(?<="authenticated":)[^,]*')
-						 local session=$(echo -e "$login" | grep -Po '(?<=sessionid=)[^;]*')
-						 local usid=$(echo -e "$login" | grep -Po '(?<=userId":")[^"]*'|sort -u)
-						#        -H "user-agent: $useragent" \
-						#        -H "x-csrftoken: $csrf" \
-						#        -H 'accept: */*' \
-						#        -H 'content-type: application/x-www-form-urlencoded' \
-						#        -H 'origin: https://www.instagram.com' \
-						#        -H 'x-requested-with: XMLHttpRequest' \
-						# login=$(curl -D - 'https://www.instagram.com/accounts/login/ajax/' \
-					 for i in "${files[@]}"
-					 gete
-					 LAM:SDJUHYWIUUUU	`32R7Udo:done
-					# consumed. Consider having the pipefail option
-					#default='/data/data/com.termux/files/home/MassLike_Instagram'
-					#find=$(find *.cookies -type f)
-					--data-urlencode "username=${1}" --data-urlencode "enc_password=#PWD_INE0QSxi":
-					STAGRAM_BROWSER:0:${time}:${2}" --data-urlencode "optIntoOneTap=false" --compressed -sL -D -)
-					-H "cookie: csrftoken=$csrf" \
-					-H "cookie: mid=$mid; ig_did=$did; csrftoken=$csrf;" \
-					-H "user-agent: $useragent" \
-					-H "x-csrftoken: $csrf" \
-					-H "x-csrftoken: $csrf" \
-					-H "x-instagram-ajax: $rolout" \
-					-H 'accept-encoding: gzip, deflate, br' \
-					-H 'accept-language: en-US,en;q=0.9' --compressed -sL)
-					-H 'accept-language: en-US,en;q=0.9' \
-					-H 'accept-language: id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7' \
-					-H 'accept: */*' --compressed)|grep -Po '(?<=__typename":"GraphImage","id":").*?(?=")'|sort -u
-					-H 'accept: */*' \
-					-H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' \
-					-H 'authority: www.instagram.com' \
-					-H 'content-type: application/x-www-form-urlencoded' \
-					-H 'content-type: application/x-www-form-urlencoded' \
-					-H 'origin: https://www.instagram.com' \
-					-H 'referer: https://www.instagram.com/' \
-					-H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36' \
-					-H 'x-ig-app-id: 936619743392459' \
-					-H 'x-requested-with: XMLHttpRequest' \
-					-H 'x-requested-with: XMLHttpRequest' \
-					-X 'POST' \
-					b=$(/bin/true|echo "${Fruits[1]}")
-					COOKIE_MY='*.cookies'
-					else
-					Fruits=('csrftoken' 'ig_did' 'sessionid')
-					Fruits[0]="csrftoken"
-					Fruits[1]="ig_did"
-					Fruits[2]="sessionid"
-					local ambil=$(curl -D - 'https://www.instagram.com/accounts/login/' \
-					local csrf=$(echo "$ambil" | grep -Po '(?<="csrf_token":").*?(?=")')
-					local did=$(echo "$ambil"|grep -Po '(?<=ig_did=)[^;]*')
-					local login=$(curl 'https://www.instagram.com/accounts/login/ajax/' -sS \
-					local mid=$(echo "$ambil"|grep -Po '(?<=mid=)[^;]*')
-					local ses=$(echo "$ambil"|grep -Po '(?<=sessionid=)[^;]*')
-					local y=$(echo "$SCRIPT_DIR/$COOKIE_MY" "$*")                                                        
-					mapfile -t oke < <(cat *.cookies)
-					p=$(/bin/true|echo "${Fruits[2]}")
-					printf "\n%s[+]%sLogining in..\n" "${H}" "${N}"
-					random
-					readarray -t files < <(compgen -G "$y")
-					set -o pipefail
-					x=$(/bin/true|echo "${Fruits[0]}")
-				 
-				 
-				 #find=$(find *.cookies -type f)
-				 fi
-				 if [[ ! -e $files ]];then                                                 
-			 function idd() { 
-			 }
-			function gaskan(){
-			idd
-			}
-		  sleep 1&:[fekifndsf\::::''""LKJHGT&duyqwic;fv'lrgewfhuocwjp;'
-		#;echo $? 
-		done
-		echo ${oke[31]}|grep -Po "(?<=$b=)[^;]*"
-		for URL in $(cat fot.txt); do
-		gaskan
-	 )
-	 -H "cookie: mid=${id};ig_did=${did};rur=VLL; csrftoken=${csrf}; ds_user_id=${usid}; sessionid=${ses}" --compressed -s
-	 -H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36" \
-	 -H "x-csrftoken: $csrf" \
-	 -H 'accept: */*' \
-	 -H 'content-type: application/x-www-form-urlencoded' \
-	 -H 'x-requested-with: XMLHttpRequest' \
-	 -X 'POST' \
-	 printf "%s[#]%s%s (mdaID) -> %s%s\n" "${H}" "${N}" "${TUSS}" "${ea}" "${N}"
-	 randomNumber=$(($RANDOM))
-	 TUSS=$(echo -e "$x" |grep -Po '(?<=status":")[^"]*')
-	 x=$(curl "https://www.instagram.com/web/likes/${ea}/like/" \
-	### MANUAL 
-	'					#        -H 'referer: https://www.instagram.com/accounts/login/' --data-urlencode "username=${1}" --data-urlencode "enc_password=#PWD_INSTAGRAM_BROWSER:0:${time}:${2}" --data-urlencode "optIntoOneTap=false" --compressed -sL)     
-	csrf=$(cat 6071.cookies| grep -Po '(?<=csrftoken=)[^;]*')
-	did=$(cat 6071.cookies|grep -Po '(?<=ig_did=)[^;]*')
-	echo -e "$v" >> NAH.txt
-	EMAIL=${array[0]}
-	EMPAS="${bacot[$i]}"
-	fi
-	for ea in $(cat NAH.txt);do
-	function gete(){
-	function open(){
-	hitung=$(($hitung+1))
-	if (( $kamu == 0 || $hitung == 0 ));
-	IFS=$'\n'
-	IFS='|' read -r -a array <<< "$EMPAS"
-	kamu=$(expr $hitung % 1)
-	local SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
-	login $EMAIL $PASS $NUMBER &
-	NUMBER=$((nomor++))
-	PASS=${array[1]}
-	ses=$(cat 6071.cookies|grep -Po '(?<=sessionid=)[^;]*')
-	set -- *  
-	then
-	v=$(open)
-	}
-	}
-   		(curl -s "${URL}" --location -A "${useragent}" \
- create="akun.txt"
- do
- done
- done
- for file in $(awk 'END {print NR}' $create)
- IFS=$(echo -en "\n\b")
- IFS=$OLDIFS
- OLDIFS=$IFS
- rm -rf NAH.txt
- unset create
-"
-":	7P9;NIKYU8-I;KHTM"
-# CREATE ON 20:06 SELESAI JAM 23:26
+#!/bin/bash
+# powered by widhisec
 # TOLONG YA HARGAI PEMBUAT NYA
-# widhisec ~ ZSECC0DE-CREW-ID
-############ encoding ##################
-############ TIME ######################
-#######################################
-########################################
-########################################
-${N}create by - ( ${B}widhisec )
-${Y}├─┤│ │ │ │ ││  │├┴┐││ ┬ | ${N}for instagram
--------------------------
-7G68O7ILUK.JB,NB NVHLGUTVI;Y/BKJL, ,HB-8YGH ############ COLORRRRRR ################
-:	"[P-00000000000000]"
-________________________
-AQa			A5ASAHTYY H#!/bin/bash
-B=$(tput setaf 6)
-declare -i hitung=0
-declare -i nomor=1
-done
-echo "Delimeter list -> email|password"
-echo -e "
-echo -e "${N}[${M}(!)${N}] ${N}LOGIN DULU BRO..."
-echo -e '-------------------------------------------'
-echo -n "Masukan File List : "
-export LC_ALL=en_US.UTF-8
-for (( i = 0; i <"${#bacot[@]}"; i++ )); do
-function login() {
-function random(){
+# UPDATE 09/12/2022 
+useragent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+############ COLORRRRRR ################
 H=$(tput setaf 2)
-IFS=$'\r\n' GLOBIGNORE='*' command eval 'bacot=($(cat $USERmailist))'
-jumlahlines=$(cat $USERmailist | wc -l <(cut -f1 -d' '))
-ls
 M=$(tput setaf 1)
 N=$(tput sgr0)
-read USERmailist;
-time=$(date +%s%N|cut -b1-13)
-useragent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-wait
+B=$(tput setaf 6)
 Y=$(tput setaf 3)
+########################################
+############ encoding ##################
+export LC_ALL=en_US.UTF-8
+#######################################
+############ TIME ######################
+time=$(date +%s%N|cut -b1-13)
+########################################
+function login() {
+	set -- *
+	local SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+	#default='/data/data/com.termux/files/home/MassLike_Instagram'
+	COOKIE_MY="$EMAIL.cookies"
+	y="$SCRIPT_DIR/$COOKIE_MY"
+	readarray -t files < <(compgen -G "$y")
+	#find=$(find *.cookies -type f)
+	if [[ ! -e $files ]];then
+		printf "\n%s[+]%sLogining in..\n" "${H}" "${N}"
+		local ambil=$(curl -D - "https://www.instagram.com/accounts/login/ajax/" -sL)
+		local csrf=$(echo "$ambil" | grep -Po '(?<=csrftoken=)[^;]*')
+		local mid=$(echo "$ambil"|grep -Po '(?<=mid=)[^;]*')
+		local did=$(echo "$ambil"|grep -Po '(?<=ig_did=)[^;]*')
+		local ses=$(echo "$ambil"|grep -Po '(?<=sessionid=)[^;]*')
+		local login=$(curl -D - 'https://www.instagram.com/api/v1/web/accounts/login/ajax/' \
+				-H 'origin: https://www.instagram.com' \
+				-H 'x-requested-with: XMLHttpRequest' \
+				-H "user-agent: $useragent" \
+				-H "x-csrftoken: $csrf" \
+				-H 'content-type: application/x-www-form-urlencoded' \
+				-H 'accept: */*' \
+			-H 'referer: https://www.instagram.com/accounts/login/' --data-urlencode "username=${EMAIL}" --data-urlencode "enc_password=#PWD_INSTAGRAM_BROWSER:0:${time}:${PASS}" --data-urlencode "optIntoOneTap=false" --compressed -sL)
+		#local check=$(echo -e "$login" | grep -Po '(?<=checkpoint_url":")[^"]*')
+		local usid=$(echo -e "$login" | grep -Po '(?<=userId":")[^"]*'|sort -u)
+		#local isauth=$(echo -e "$login" | grep -Po '(?<="authenticated":)[^,]*')
+		#local session=$(echo -e "$login" | grep -Po '(?<=sessionid=)[^;]*')			
+		echo "${login}" >> $EMAIL.cookies
+		echo "ID -> ${usid}"
+		gete
+	else
+		rm -rf $EMAIL.cookies
+		#for i in "${files[@]}";do echo "Found -$i-";done
+	fi
+
+
 }
+function idd() {
+    gaskan
+	(curl -s "${URL}" --location -A "${useragent}" \
+			-H 'accept-language: en-US,en;q=0.9' \
+			-H 'x-requested-with: XMLHttpRequest' \
+			-H "x-csrftoken: $csrf" \
+			-H "cookie: mid=${id};ig_did=${did};rur=VLL; csrftoken=${csrf}; sessionid=${ses}" \
+			-H 'content-type: application/x-www-form-urlencoded' \
+		-H 'accept: */*' --compressed)|grep -Po '(?<="media_id":").*?(?=")'|sort -u 
 }
+function gaskan(){
+	set -o pipefail
+	Fruits=('csrftoken' 'ig_did' 'sessionid')
+	Fruits[0]="csrftoken"
+	Fruits[1]="ig_did"
+	Fruits[2]="sessionid"
+	z=$(/bin/true|echo "${Fruits[0]}")
+	b=$(/bin/true|echo "${Fruits[1]}")
+	p=$(/bin/true|echo "${Fruits[2]}")
+	#mapfile -t oke < <(cat *.cookies)	
+	#;echo $?
+}
+function open(){
+	for URL in $(cat fot.txt); do
+		idd
+	done
+}
+function gete(){
+	gaskan	
+	 csrf=$(cat $EMAIL.cookies| grep -Po "(?<=$z=)[^;]*"|sort -u)
+	 did=$(cat $EMAIL.cookies|grep -Po "(?<=$b=)[^;]*"|sort -u)
+	 ses=$(cat $EMAIL.cookies|grep -Po "(?<=$p=)[^;]*"|sort -u)
+	v=$(open)
+	echo -e "$v" >> NAH.txt
+	for ea in $(cat NAH.txt);do
+		x=$(curl "https://www.instagram.com/web/likes/$ea/like/" \
+				-X 'POST' \
+				-H 'content-type: application/x-www-form-urlencoded' \
+				-H 'accept: */*' \
+				-H 'x-requested-with: XMLHttpRequest' \
+				-H "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36" \
+				-H "x-csrftoken: $csrf" \
+				-H "cookie: mid=${id};ig_did=${did};rur=VLL; csrftoken=${csrf}; ds_user_id=${usid}; sessionid=${ses}"  --compressed -sL
+ 		)
+		TUSS=$(echo -e "$x" |grep -Po '(?<=status":")[^"]*')
+		printf "%s[#]%s%s (mdaID) -> %s%s\n" "${H}" "${N}" "${TUSS}" "${ea}" "${N}"
+	done
+	rm -rf NAH.txt
+}
+echo -e "
+${N}create by - ( ${B}widhisec )
+________________________
 ┌─┐┬ ┬┌┬┐┌─┐┬  ┬┬┌─┬┌─┐ |
+${Y}├─┤│ │ │ │ ││  │├┴┐││ ┬ | ${N}for instagram
 ┴ ┴└─┘ ┴ └─┘┴─┘┴┴ ┴┴└─┘ |
+-------------------------
+"
+ls
+echo -e '-------------------------------------------'
+echo -e "${N}[${M}(!)${N}] ${N}LOGIN DULU BRO..."
+echo "Delimeter list -> email|password"
+echo -n "Masukan File List : "
+read USERmailist;
+declare -i nomor=1
+jumlahlines=$(cat $USERmailist | cut -f1 -d' ')
+declare -i hitung=0
+IFS=$'\r\n' GLOBIGNORE='*' command eval 'bacot=($(cat $USERmailist))'
+for (( i = 0; i <"${#bacot[@]}"; i++ )); do
+	EMPAS="${bacot[$i]}"
+	IFS='|' read -r -a array <<< "$EMPAS"
+	EMAIL=${array[0]}
+	PASS=${array[1]}
+	NUMBER=$((nomor++))
+	kamu=$(expr $hitung % 1)
+	if (( $kamu == 0 || $hitung == 0 ));
+	then
+		sleep 1
+	fi
+	login $EMAIL $PASS $NUMBER &
+	hitung=$(($hitung+1))
+done
+wait
